1e990f2cfa54cf1ae91e0adf5e095007b5d86549
Lunsynum1 <lunselod87@gmail.com>
2025-10-14 12:53:58 +0500

    1 Les Components basics

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,36 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import { Component } from 'react'
 import './App.css'
 
+const Header = () => {
+  return <h2>Hello world!</h2>
+}
+
+// const Field = () => {
+//   const holder = "Enter here";
+//   const styledField = {
+//     width: '300px',
+//     height: '40px',
+//   };
+//   return <input
+//     placeholder={holder}
+//     type="text"
+//     style={styledField} />
+// }
+
+class Field2 extends Component {
+  render() {
+    const holder = "Enter here";
+    const styledField = {
+      width: '300px',
+      height: '40px',
+    };
+    return <input
+      placeholder={holder}
+      type="text"
+      style={styledField} />
+  }
+}
+
+function Btn() {
+  const text = 'Log in';
+  const logged = false;
+
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Welcome! My Lord!</h1>
-      <h2>Vite + React ready to serve you</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Field2 />
+      <Btn />
     </>
   )
 }
 
+export { Header };
 export default App
--- my-first-app/src/main.jsx
+++ my-first-app/src/main.jsx
@@ -2,17 +2,9 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Header } from './App.jsx';
 
-const text = 'Hello World!';
-
-const elem = (
-  <div>
-    <h2>Text: {text}</h2>
-    <input type="text" />
-    <button />
-  </div>
-);
 
 createRoot(document.getElementById('root')).render(
-  elem
+  <App />,
 )
