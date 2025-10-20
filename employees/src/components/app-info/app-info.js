30cbe9d0b525ea704e074b69ce48c59aa0bc81fc
Lunsynum1 <lunselod87@gmail.com>
2025-10-20 17:51:07 +0500

    fist page stage

--- employees/src/components/app-info/app-info.js
+++ employees/src/components/app-info/app-info.js
@@ -1,2 +1,12 @@
+import './app-info.css';
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании L</h1>
+            <h2>Общие число сотрудников</h2>
+            <h3>Премию получат: </h3>
+        </div>
+    );
+}
 
-
+export default AppInfo;
