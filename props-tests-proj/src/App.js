1ea29e0cb494b06327e68bcf42bcb7e2602eba3f
Lunsynum1 <lunselod87@gmail.com>
2025-10-28 14:25:20 +0500

    lesson props ended

--- props-tests-proj/src/App.js
+++ props-tests-proj/src/App.js
@@ -1,10 +1,21 @@
 
-import './app.css';
+import './App.css';
+
+function WhoAmI({ name, surname, link }) {
+    return (
+        <div>
+            <h1>My name is {name()}, surname - {surname}</h1>
+            <a href={link}>My profile</a>
+        </div>
+    );
+}
 
 function App() {
     return (
         <div className="App">
-            <h1>React props test</h1>
+            <WhoAmI name={() => { return 'John' }} surname="Smith" link="facebook.com/johnsmith" />
+            <WhoAmI name={() => { return 'Alex' }} surname="Johnson" link="facebook.com/alexjohnson" />
+            <WhoAmI name={() => { return 'Sarah' }} surname="Williams" link="facebook.com/sarahwilliams" />
         </div>
     );
 }
